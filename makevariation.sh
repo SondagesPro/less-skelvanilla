19111f1279a3adcd5a512263aebcf6ee656e4f23
Denis Chenu <denis@sondages.pro>
2018-12-22 21:05:23 +0100

    [fix] rename cosmos [feature] make variation allow morevariables (in option)

--- makevariation.sh
+++ makevariation.sh
@@ -5,15 +5,25 @@ mkdir variations
 for variable in variations-variables/*.less
 do
     cat ${variable} > variables.less
+    variation=${variable##*/}
+    variation=${variation%.*}
+    cat variations-morevariables/contrast.less > morevariables.less
+    if [ -r variations-morevariables/${variation}.less ];
+    then
+        cat variations-morevariables/${variation}.less > morevariables.less
+    fi
     for theme in "${themes[@]}"
     do
-        themename=${variable##*/}
-        themename=${themename%.*}
-        themename=${theme/theme/$themename}
+        themename=${theme/theme/$variation}
         lessc ${theme}.less variations/${themename}.css
         cleancss variations/${themename}.css > variations/${themename}.min.css
         rm variations/${themename}.css
     done
+    echo "variations ${variation} done";
 done
+# reset to default
 cat variations-variables/contrast.less > variables.less
+cat variations-morevariables/contrast.less > morevariables.less
+lessc --source-map --source-map-rootpath=https://gitlab.com/SondagesPro/SurveyThemes/less-limesurvey/raw/master/ theme-button.less lsbootstrap.css
+
 echo "All variations done, you can use at variations with skelvanilla"
